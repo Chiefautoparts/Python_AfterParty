21a36a3ac8fc80c2134041ae933d8c9bca1d043f
chiefautoparts <chiefautoparts@outlook.com>
2017-07-06 15:44:13 -0700

    w0rk

---
+++ fundamentals/dictionaryBasics.py
@@ -0,0 +1,6 @@
+me = {"name": "max", "age": "25", "birth": "'Merica", "language": "Python"}
+
+print "My name is " + me["name"]
+print "My age is " + me["age"]
+print "My country of birth is " + me["birth"]
+print "My favorite language is " + me["language"]
---
+++ fundamentals/strings.py
@@ -0,0 +1,13 @@
+words = "It's thanksgiving day. It's my birthday, too!"
+
+spot = words.find('day')
+
+#print spot
+newStr = words.replace('day', 'month')
+
+#print newStr
+
+num = [2, 54, -2, 7, 12, 98]
+
+#print min(num)
+#print max(num)
