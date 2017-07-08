a6fb38bfd908d06297f14a82a23bd00275437c3c
chiefautoparts <chiefautoparts@outllook.com>
2017-07-08 10:50:50 -0700

    flask

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-	return redner_template('index.html')
+	return redner_template('index.html', name ='Max')
 
 @app.route('/success')
 def success():
