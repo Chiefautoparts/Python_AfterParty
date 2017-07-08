d3a800528d965393f9b2794ae50bf834bdd8b000
chiefautoparts <chiefautoparts@outllook.com>
2017-07-07 22:32:48 -0700

    OOP priducts

--- OOP/product.py
+++ OOP/product.py
@@ -7,9 +7,31 @@ class product(object):
 		self.brand = brand
 		self.cost = cost
 		self.status = 'for sale'
+		self.reason = 'reason'
 
 	def sell(self):
 		self.status = 'sold'
 		return self
 
-	def
+	def tax(self):
+		self.price = (self.price * 1.1)
+		print self.price
+		return self
+
+	def exchange(self, reason):
+		if self.reason == 'defective':
+			self.price = 0
+		elif self.reason == 'new in box':
+			self.status = 'For sale'
+		elif self.reason == 'opened in box':
+			self.status = 'used'
+			self.price = (self.price * .8)
+		return self
+
+	def display(self):
+		print self.price, self.name, self.weight, self.brand, self.status
+		return self
+
+glass = product(5, 'glass', 2, 'nike', 1)
+
+glass.display()
