ec360751dfca120557fcf45f3ba618245d6160db
chiefautoparts <chiefautoparts@outllook.com>
2017-07-11 11:06:19 -0700

    random_word

--- Django/random_word/apps/wordGen/views.py
+++ Django/random_word/apps/wordGen/views.py
@@ -4,10 +4,22 @@ import random
 
 # Create your views here.
 def  index(request):
+	return render(request, 'wordGen/index.html')
+
+def generator(request):
+	if 'counter' in request.session:
+		request.session['counter'] += 1
+	else:
+		request.session['counter'] = 1 
+	word = ''
 	chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-	word = ''.join(random.choice(chars)for _ in range(10))
-	request.session['counter'] = 0
-	counter += 1
+	for times in range(0,14):
+		word = word + str(random.choice(chars))
+	words = {
+		'randomWord': word
+	}
+	return render(request, 'wordGen/index.html', words)
+	
 
 
-	return render(request, 'wordGen/index.htm')
+	# return render(request, 'wordGen/index.htm')
