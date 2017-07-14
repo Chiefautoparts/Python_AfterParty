2dc3fc882db5d6d27c09360770e1cb4961a7b01a
chiefautoparts <chiefautoparts@outllook.com>
2017-07-14 11:49:41 -0700

    book/author app

--- Django/dojo_ninjas/dojo_ninjas/settings.py
+++ Django/dojo_ninjas/dojo_ninjas/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.dojoApp',
+    'apps.book_author',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Django/dojo_ninjas/dojo_ninjas/urls.py
+++ Django/dojo_ninjas/dojo_ninjas/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('apps.dojoApp.urls'))
+    url(r'^', include('apps.dojoApp.urls')),
+    url(r'^', include('apps.book_author.urls'))
 ]
