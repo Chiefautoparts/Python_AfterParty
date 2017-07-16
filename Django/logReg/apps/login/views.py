8417e560fa2806b527fb6d57b05b41d5d8cb7762
chiefautoparts <chiefautoparts@outllook.com>
2017-07-15 23:26:12 -0700

    logReg work

--- Django/logReg/apps/login/views.py
+++ Django/logReg/apps/login/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render, redirect
 def index(request):
 	return render(request, 'login/index.html')
 
+def  register(request):
+
 # Create your views here.
+def login(request):
+
+
+def success(request):
