6e2d5ca4b32a0b49ebe06587133e03c2f3c76c03
chiefautoparts <chiefautoparts@outllook.com>
2017-07-24 08:59:32 -0700

    pushing a bunch of work done offline

--- Django/logReg/apps/login/views.py
+++ Django/logReg/apps/login/views.py
@@ -1,33 +1,36 @@
 from __future__ import unicode_literals
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, HttpResponse, redirect 
 from .models import User
 from django.contrib import messages
 
 
 def index(request):
-	if request.session.get('id'):
-		return render(request, 'login/success.html')
+	
 	return render(request, 'login/index.html')
 
 def register(request):
-	logged = User.objects.registerValidation(request.POST)
-	if not logged['status']:
-		for error in logged['errors']:
+	status = User.objects.registerValidation(request.POST)
+	if not status['valid']:
+		for error in status['errors']:
 			messages.error(request, error)
-		return redirect('/')
-	request.session['id'] = logged['user'].id
-	return redirect('/success')
+	else:
+		messages.success(request, 'User createed,  login please')
+	
+	return redirect('auth:index')
 
 def login(request):
-	logged = User.objects.loginValidation(request.POST)
-	if not logged['status']:
-		for error in logged['errors']:
+	status = User.objects.loginValidation(request.POST)
+	if not status['valid']:
+		for error in status['errors']:
 			messages.error(request, error)
-		return redirect('/')
 	else:
-		user = User.objects.get(id=logged['user'])
-		request.session['id'] = user.id
-	return redirect('/success')
+		request.session['id'] = status['user'].id
+		return redirect('auth:success')
+	return redirect('/')
+
+def logout(request):
+	request.session.clear()
+	return redirect('auth:index')
 
 def success(request):
 	user = User.objects.get(id=request.session['id'])
